b8e1d4fe1a90230b2dc53d9538dcf4d9a7826745
GowthamGirithar <g.srirangasamy@sap.com>
2017-09-20 22:11:09 +0530

    added the files

---
+++ java-project/src/test/java/com/gg/ml/DecisionTreeDemoTest.java
@@ -0,0 +1,12 @@
+package com.gg.ml;
+import org.junit.Test;
+
+
+public class DecisionTreeDemoTest {
+	
+	@Test
+	public void test() throws Exception  {
+          DecisionTreeDemo.process();
+	}
+
+}
